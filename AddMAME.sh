afd245f6ffa0cb3f8328dc8f70e524fce6a236fe
Alan Cooper <ScallyCoops@gmail.com>
2020-05-28 21:37:34 +0100

    Update bash file

--- AddMAME.sh
+++ AddMAME.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+ 
 # Distribute the files required for MAME-4-PicoChess
-
 clear
 echo "This will not work alongside my new Switchable Image as the folder names have changed"
 echo "Hit CTRL C to cancel this Bash script"
@@ -13,25 +13,32 @@ sudo service picochess stop
 wait
 
 # Move mame_emulation to /opt/picochess/engines/
-cd ~/MAME-4-PicoChess/mame_emulation
+cd ~/MAME-4-PicoChess/mame_emulation_new
 echo ""
 echo "Unzipping mess & copying mame_emulation to /opt/picochess/engines"
 sleep 1
-sudo unzip mess.zip
+unzip mess.zip
 wait
 rm mess.zip
 ./mess -cc 
 cd ~/MAME-4-PicoChess
-sudo cp -r mame_emulation /opt/picochess/engines
+cp -r mame_emulation_new /opt/picochess/engines
 
-# Rename your existing armv7l to armv7lPICO and copy the armv7l-MAME file to /opt/picochess/engines
+# Rename your existing armv7l to armv7lPICO and copy the armv7l-MAME-new file to /opt/picochess/engines
 echo ""
-echo "Renaming your existing armv7l to armv7lPICO (unless it exists) and applying the armv7l-MAME folder"
+echo "Moving your existing armv7l to armv7lPICO (unless it exists) & replacing with the new armv7l-MAME folder"
 sleep 1
-sudo cp -r armv7l-MAME /opt/picochess/engines
+cp -r armv7l-MAME-new /opt/picochess/engines
 cd /opt/picochess/engines
-sudo mv -vn armv7l armv7lPICO
-sudo cp -r armv7l-MAME armv7l
+mv -vn armv7l armv7lPICO
+mv armv7l-MAME-new armv7l
+
+echo ""
+echo "Moving any existing mame_emulation to mame_emulation_old & replacing with the new mame_emulation folder"
+sleep 1
+mv mame_emulation mame_emulation_old
+mv mame_emulation_new mame_emulation
+
 
 echo ""
 echo "You can revert to your existing engines by deleting armv7l & copying armv7lPICO to armv7l"
